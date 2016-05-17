1f9af5cd2732beacc684a87d611d5be4b6f21c29
svitcode <yarik.proger@gmail.com>
2016-05-17 11:06:46 +0300

    add index controller

---
+++ src/Application/Controller/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Application\Controller;
+
+use Tlumx\Controller;
+
+class IndexController extends Controller
+{
+    public function indexAction()
+    {
+        echo "index action";
+    }
+}
