fbc7118f8bcc22103746ce65ea3a83b91fdaeb3a
svitcode <yarik.proger@gmail.com>
2016-05-17 13:09:17 +0300

    add bootstraper

--- configs/main.php
+++ configs/main.php
@@ -1,6 +1,9 @@
 <?php
 
 return [
+    'bootstrappers' => [
+        'Application\Bootstrap'
+    ],    
     'controllers' => [
         'index' => 'Application\Controller\IndexController'
     ],
---
+++ src/Application/Bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Application;
+
+use Tlumx\Bootstrapper;
+
+/**
+ * You can create methods init(), preRouting(), postRouting(),
+ * preDispatch(), postDispatch().
+ */
+class Bootstrap extends Bootstrapper
+{
+    public function init()
+    {
+        // TODO
+    }        
+}
