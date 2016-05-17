ca0493c44416ab245f7dcde9742f0ee9f465442f
svitcode <yarik.proger@gmail.com>
2016-05-17 10:59:05 +0300

    .

--- public/index.php
+++ public/index.php
@@ -1,9 +1,13 @@
 <?php
 
+define('DS', DIRECTORY_SEPARATOR);
+define('ROOT_PATH', dirname(__DIR__));
+define('TEMPLATES_PATH', ROOT_PATH . DS . 'templates');
+
 chdir(dirname(__DIR__));
 require 'load.php';
 
 use Tlumx\Application;
           
-$app = new Application();
+$app = new Application(include ROOT_PATH . DS . 'configs' . DS . 'main.php');
 $app->run();
