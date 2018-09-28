8019fb2cd1d76b043f32b417732759013748ec76
Yaroslav Kharitonchuk <yarik.proger@gmail.com>
2018-09-28 18:27:45 +0300

    modify index.php

--- public/index.php
+++ public/index.php
@@ -3,11 +3,12 @@
  * Tlumx (https://tlumx.com/)
  *
  * @author    Yaroslav Kharitonchuk <yarik.proger@gmail.com>
- * @link      https://github.com/tlumx/framework
+ * @link      https://github.com/tlumx/TlumxSkeletonApplication
  * @copyright Copyright (c) 2016-2018 Yaroslav Kharitonchuk
- * @license   https://github.com/tlumx/framework/blob/master/LICENSE.md  (MIT License)
+ * @license   https://github.com/tlumx/TlumxSkeletonApplication/blob/master/LICENSE.md  (MIT License)
  */
 
+define('DEBUG', true);
 define('ROOT_PATH', dirname(__DIR__));
 define('TEMPLATES_PATH', ROOT_PATH . '/templates');
 
@@ -17,7 +18,7 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use Tlumx\Application\Application;
 
-$config = include 'configs/main.php';
+$config = include 'config/main.php';
 
 $app = new Application($config);
 $app->run();
