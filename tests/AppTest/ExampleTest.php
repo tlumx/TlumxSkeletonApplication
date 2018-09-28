de74a85f673909a7a96d19540eee0e8628ff2423
Yaroslav Kharitonchuk <yarik.proger@gmail.com>
2018-09-28 18:24:36 +0300

    Add simple PHPUnit test

---
+++ tests/AppTest/ExampleTest.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Tlumx (https://tlumx.com/)
+ *
+ * @author    Yaroslav Kharitonchuk <yarik.proger@gmail.com>
+ * @link      https://github.com/tlumx/TlumxSkeletonApplication
+ * @copyright Copyright (c) 2016-2018 Yaroslav Kharitonchuk
+ * @license   https://github.com/tlumx/TlumxSkeletonApplication/blob/master/LICENSE.md  (MIT License)
+ */
+namespace AppTest;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
